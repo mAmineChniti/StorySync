1c565d6bbbdddefc6ea90f81244c61d8b186ddca
mAmineChniti <emin.chniti@esprit.tn>
2025-03-29 15:15:19 +0100

    fixed wrong url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,31 +3,31 @@ import { type MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://storysync.vercel.app",
+      url: "https://storysync-delta.vercel.app",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://storysync.vercel.app/register",
+      url: "https://storysync-delta.vercel.app/register",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://storysync.vercel.app/login",
+      url: "https://storysync-delta.vercel.app/login",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://storysync.vercel.app/terms-of-service",
+      url: "https://storysync-delta.vercel.app/terms-of-service",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
     },
     {
-      url: "https://storysync.vercel.app/privacy-policy",
+      url: "https://storysync-delta.vercel.app/privacy-policy",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
